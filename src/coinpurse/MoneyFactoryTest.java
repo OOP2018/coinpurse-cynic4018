e01d08e75c1143cba47c3f6b8d62bd340dae7df4
Katapon Sinpunyawong <32285642+cynic4018@users.noreply.github.com>
2018-02-23 23:02:25 +0700

    Fixed Lab5

--- src/coinpurse/MoneyFactoryTest.java
+++ src/coinpurse/MoneyFactoryTest.java
@@ -41,11 +41,11 @@ public class MoneyFactoryTest {
 		assertEquals(new Coin(5, thCURRENCY), th.createMoney(5));
 		assertEquals(new Coin(10, thCURRENCY), th.createMoney(10));
 		//test create banknote.
-		assertEquals(new BankNote(20, thCURRENCY), th.createMoney(20));
-		assertEquals(new BankNote(50, thCURRENCY), th.createMoney(50));
-		assertEquals(new BankNote(100, thCURRENCY), th.createMoney(100));
-		assertEquals(new BankNote(500, thCURRENCY), th.createMoney(500));
-		assertEquals(new BankNote(1000, thCURRENCY), th.createMoney(1000));
+		assertEquals(new BankNote(20, thCURRENCY, 1000000), th.createMoney(20));
+		assertEquals(new BankNote(50, thCURRENCY, 1000001), th.createMoney(50));
+		assertEquals(new BankNote(100, thCURRENCY, 1000002), th.createMoney(100));
+		assertEquals(new BankNote(500, thCURRENCY, 1000003), th.createMoney(500));
+		assertEquals(new BankNote(1000, thCURRENCY, 1000004), th.createMoney(1000));
 		
 		//use createMoney(String value)
 		//test create coin.
@@ -54,11 +54,11 @@ public class MoneyFactoryTest {
 		assertEquals(new Coin(5, thCURRENCY), th.createMoney("5"));
 		assertEquals(new Coin(10, thCURRENCY), th.createMoney("10"));
 		//test create banknote.
-		assertEquals(new BankNote(20, thCURRENCY), th.createMoney("20"));
-		assertEquals(new BankNote(50, thCURRENCY), th.createMoney("50"));
-		assertEquals(new BankNote(100, thCURRENCY), th.createMoney("100"));
-		assertEquals(new BankNote(500, thCURRENCY), th.createMoney("500"));
-		assertEquals(new BankNote(1000, thCURRENCY), th.createMoney("1000"));	
+		assertEquals(new BankNote(20, thCURRENCY, 1000005), th.createMoney("20"));
+		assertEquals(new BankNote(50, thCURRENCY, 1000006), th.createMoney("50"));
+		assertEquals(new BankNote(100, thCURRENCY, 1000007), th.createMoney("100"));
+		assertEquals(new BankNote(500, thCURRENCY, 1000008), th.createMoney("500"));
+		assertEquals(new BankNote(1000, thCURRENCY, 1000009), th.createMoney("1000"));	
 	}
 	
 	/**
@@ -77,13 +77,13 @@ public class MoneyFactoryTest {
 		assertEquals(new Coin(20, "Sen"), ml.createMoney(0.20));
 		assertEquals(new Coin(50, "Sen"), ml.createMoney(0.50));
 		//test create banknote.
-		assertEquals(new BankNote(1, mlCURRENCY), ml.createMoney(1));
-		assertEquals(new BankNote(2, mlCURRENCY), ml.createMoney(2));
-		assertEquals(new BankNote(5, mlCURRENCY), ml.createMoney(5));
-		assertEquals(new BankNote(10, mlCURRENCY), ml.createMoney(10));
-		assertEquals(new BankNote(20, mlCURRENCY), ml.createMoney(20));
-		assertEquals(new BankNote(50, mlCURRENCY), ml.createMoney(50));
-		assertEquals(new BankNote(100, mlCURRENCY), ml.createMoney(100));
+		assertEquals(new BankNote(1, mlCURRENCY, 1000000), ml.createMoney(1));
+		assertEquals(new BankNote(2, mlCURRENCY, 1000001), ml.createMoney(2));
+		assertEquals(new BankNote(5, mlCURRENCY, 1000002), ml.createMoney(5));
+		assertEquals(new BankNote(10, mlCURRENCY, 1000003), ml.createMoney(10));
+		assertEquals(new BankNote(20, mlCURRENCY, 1000004), ml.createMoney(20));
+		assertEquals(new BankNote(50, mlCURRENCY, 1000005), ml.createMoney(50));
+		assertEquals(new BankNote(100, mlCURRENCY, 1000006), ml.createMoney(100));
 		
 		//use createMoney(String value)
 		//test create coin.
@@ -92,20 +92,16 @@ public class MoneyFactoryTest {
 		assertEquals(new Coin(20, "Sen"), ml.createMoney("0.20"));
 		assertEquals(new Coin(50, "Sen"), ml.createMoney("0.50"));
 		//test create banknote.
-		assertEquals(new BankNote(1, mlCURRENCY), ml.createMoney("1"));
-		assertEquals(new BankNote(2, mlCURRENCY), ml.createMoney("2"));
-		assertEquals(new BankNote(5, mlCURRENCY), ml.createMoney("5"));
-		assertEquals(new BankNote(10, mlCURRENCY), ml.createMoney("10"));
-		assertEquals(new BankNote(20, mlCURRENCY), ml.createMoney("20"));
-		assertEquals(new BankNote(50, mlCURRENCY), ml.createMoney("50"));
-		assertEquals(new BankNote(100, mlCURRENCY), ml.createMoney("100"));
+		assertEquals(new BankNote(1, mlCURRENCY, 1000007), ml.createMoney("1"));
+		assertEquals(new BankNote(2, mlCURRENCY, 1000008), ml.createMoney("2"));
+		assertEquals(new BankNote(5, mlCURRENCY, 1000009), ml.createMoney("5"));
+		assertEquals(new BankNote(10, mlCURRENCY, 1000010), ml.createMoney("10"));
+		assertEquals(new BankNote(20, mlCURRENCY, 1000011), ml.createMoney("20"));
+		assertEquals(new BankNote(50, mlCURRENCY, 1000012), ml.createMoney("50"));
+		assertEquals(new BankNote(100, mlCURRENCY, 1000013), ml.createMoney("100"));
 		
 	}
-	
-
-	
-		
-		
 		
 	
 }
+
